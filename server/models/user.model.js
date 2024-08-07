f259492892a69d2459bc17a10fc9284e9349a738
IsidoraStanculovic <stanculovicisidora@gmail.com>
2024-08-07 04:11:04 +0200

    Register

---
+++ server/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePhoto: {
+      type: String,
+      default: "",
+    },
+    coverPhoto: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: []
+        }
+    ],
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: []
+        }
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+import authRoutes from "./routes/auth.routes.js";
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+dotenv.config();
+app.use(express.json());
 
-app.listen(5000, () => console.log("Server listening on post 5000"));
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server listening on port ${PORT}`);
+});
