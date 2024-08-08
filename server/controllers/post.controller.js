0730ed6a479d878953c84cbd966bb15d93e9b232
IsidoraStanculovic <stanculovicisidora@gmail.com>
2024-08-08 02:17:07 +0200

    Delete post and get posts by author id

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,34 +1,73 @@
 import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
 
-export const create = async(req, res) => {
-    try {
-        const {content, photo} = req.body;
-        const authorId = req.user._id;
-        
-        if(!content && !photo) {
-            return res.status(400).json({error: "You must post either content or photo, or both"});
-        }
+export const create = async (req, res) => {
+  try {
+    const { content, photo } = req.body;
+    const authorId = req.user._id;
 
-        const newPost = new Post({
-            content: content, 
-            photo: photo, 
-            author: authorId
-        });
-
-        if(newPost) {
-            await newPost.save();
-            res.status(201).json({
-                _id: newPost._id,
-                content: newPost.content, 
-                photo: newPost.photo, 
-                author: newPost.author,
-                createdAt: newPost.createdAt
-            });
-        }
-        
-    } catch(error) {
-        console.log("Error in post controller, create function: ", error.message);
-        res.status(500).json({error: "Internal Server Error"});
+    if (!content && !photo) {
+      return res
+        .status(400)
+        .json({ error: "You must post either content or photo, or both" });
     }
-}
 
+    const newPost = new Post({
+      content: content,
+      photo: photo,
+      author: authorId,
+    });
+
+    if (newPost) {
+      await newPost.save();
+      res.status(201).json({
+        _id: newPost._id,
+        content: newPost.content,
+        photo: newPost.photo,
+        author: newPost.author,
+        createdAt: newPost.createdAt,
+      });
+    }
+  } catch (error) {
+    console.log("Error in post controller, create function: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { userId } = req.user._id;
+
+    const post = Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    if (post.author !== userId) {
+      return res.status(400).json({ error: "You are not the author" });
+    }
+
+    await post.deleteOne({ _id: postId });
+    res.status(200).json({ message: "Post deleted" });
+  } catch (error) {
+    console.log("Error in post controller, delete function: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getAllByAuthorId = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const posts = await Post.find({ author: userId });
+    res.status(200).json({posts});
+  } catch (error) {
+    console.log("Error in post controller, getAllByUserId function: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { create } from "../controllers/post.controller.js";
+import { create, deletePost, getAllByAuthorId } from "../controllers/post.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, create);
+router.delete("/:postId", protectRoute, deletePost);
+router.get("/user-posts/:userId", protectRoute, getAllByAuthorId);
 
 export default router;
